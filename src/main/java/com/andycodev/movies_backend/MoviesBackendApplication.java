5e19b676ac736fbf0de87f67b313d2f7391bdae7
andyvega <andy.vega@adventistas.org>
2024-12-19 14:34:01 -0500

    commit test end

--- src/main/java/com/andycodev/movies_backend/MoviesBackendApplication.java
+++ src/main/java/com/andycodev/movies_backend/MoviesBackendApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MoviesBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesBackendApplication.class, args);
 	}
-
 }
