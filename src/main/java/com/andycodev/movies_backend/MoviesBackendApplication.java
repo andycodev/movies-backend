b907deab5c60011830d59662fb16af61ef2c215b
andyvega <andy.vega@adventistas.org>
2024-12-20 13:08:27 -0500

    feat: config variables de entorno.

--- src/main/java/com/andycodev/movies_backend/MoviesBackendApplication.java
+++ src/main/java/com/andycodev/movies_backend/MoviesBackendApplication.java
@@ -2,10 +2,27 @@ package com.andycodev.movies_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MoviesBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesBackendApplication.class, args);
+	}
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
 	}
 }
