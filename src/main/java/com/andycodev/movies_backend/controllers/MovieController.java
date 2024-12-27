134e1dc660a76e5fd87fb2a37dc2bd9ece6af0dd
andyvega <andy.vega@adventistas.org>
2024-12-26 20:12:11 -0500

    feat: create, update, delete movies

--- src/main/java/com/andycodev/movies_backend/controllers/MovieController.java
+++ src/main/java/com/andycodev/movies_backend/controllers/MovieController.java
@@ -3,11 +3,13 @@ package com.andycodev.movies_backend.controllers;
 import com.andycodev.movies_backend.models.Movie;
 import com.andycodev.movies_backend.repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/movies")
@@ -16,12 +18,45 @@ public class MovieController {
     @Autowired
     private MovieRepository movieRepository;
 
+    @CrossOrigin
     //@GetMapping("/list") si deseo ampliar mi url
     @GetMapping
-    public List<Movie> getAllMovies() {
+    public List<Movie> getMovies() {
         return movieRepository.findAll();
     }
 
+    @CrossOrigin
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getMovieById(@PathVariable Long id) {
+      Optional<Movie> movie = movieRepository.findById(id);
+      return movie.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
+    @CrossOrigin
+    @PostMapping
+    public ResponseEntity<Movie> createMovie(@RequestBody Movie movie) {
+        Movie saveMovie = movieRepository.save(movie);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saveMovie);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Movie> updateMovie(@PathVariable Long id, @RequestBody Movie movie) {
+        if(!movieRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        movie.setMovieId(id);
+        Movie saveMovie = movieRepository.save(movie);
+        return ResponseEntity.ok(saveMovie);
+    }
+
+    @CrossOrigin
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMovie(@PathVariable Long id) {
+        if(!movieRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        movieRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
--- src/main/java/com/andycodev/movies_backend/repositories/MovieRepository.java
+++ src/main/java/com/andycodev/movies_backend/repositories/MovieRepository.java
@@ -3,6 +3,4 @@ package com.andycodev.movies_backend.repositories;
 import com.andycodev.movies_backend.models.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-
-}
+public interface MovieRepository extends JpaRepository<Movie, Long> {}
